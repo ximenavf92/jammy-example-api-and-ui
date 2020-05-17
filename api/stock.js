f69ef9305f2b4bd6bd03800388ee5f3e55cf11d9
Phil Hawksworth <phil@netlify.com>
2020-05-17 23:45:16 +0100

    add fake latency

--- api/stock.js
+++ api/stock.js
@@ -1,27 +1,17 @@
-var products = require('./data/catalogue.json');
 var inventory = require('./data/inventory.json');
 
 exports.handler = function(event, context, callback) {
 
-  let data = products[event.queryStringParameters.productId];
-  let availability = inventory[event.queryStringParameters.productId];
+  // get the inventory details for this product
+  let data = inventory[event.queryStringParameters.productId];
 
-  // Introduce outrageous price gauging.
-  if(availability.stock == 0) {
-    data.stock = availability.stock;
-    data.price = data.rrp
-  }
-  else if(availability.stock < 10) {
-    data.stock = "Last few items";
-    data.price = ((10 - availability.stock) * data.rrp ).toFixed(2);
-  } else {
-    data.stock = availability.stock;
-    data.price = data.rrp
-  }
-
-  callback(null, {
-    statusCode: 200,
-    body: JSON.stringify(data)
-  });
+  // add some fake latency,
+  // so we can see what's going on.
+  setTimeout(() => {
+    callback(null, {
+      statusCode: 200,
+      body: JSON.stringify(data)
+    });
+  }, 2000)
 
 }
--- src/js/shop.js
+++ src/js/shop.js
@@ -1,19 +1,19 @@
 const showStockLevels = function() {
 
   const productViews = document.querySelectorAll('.product');
-
   productViews.forEach(element => {
     fetch(`/api/stock/${element.dataset.productId}`)
       .then(response => response.json())
       .then(data => {
 
-        if(data.stock) {
-          html = `<a href="#" class="btn">Buy now</a>`;
-        } else {
+        if(!data.stock) {
           html = `<span>😕More soon!</span> `;
+        } else if (data.stock < 10) {
+          html = `<a href="#" class="btn">Just s few left! Buy now</a>`;
+        } else {
+          html = `<a href="#" class="btn">Buy now</a>`;
         }
         element.querySelector('.purchase').innerHTML = html;
-        element.querySelector('.price').innerText = data.price;
 
       })
   });
