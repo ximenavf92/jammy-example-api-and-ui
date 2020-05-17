76b1fcdfb95e868296cc01e49b50a224d3121ba3
Phil Hawksworth <phil@netlify.com>
2020-05-18 00:48:50 +0100

    no timeout

--- api/stock.js
+++ api/stock.js
@@ -5,11 +5,9 @@ exports.handler = function(event, context, callback) {
   // get the inventory details for this product
   let data = inventory[event.queryStringParameters.productId];
 
+  callback(null, {
+    statusCode: 200,
+    body: JSON.stringify(data)
+  });
 
-  setTimeout(() => {
-    callback(null, {
-      statusCode: 200,
-      body: JSON.stringify(data)
-    });
-  }, 2000);
 }
